14bf5403a40ee065a02c2c2a1f50249f02eb375d
Changmin Ding <changmin.ding@gdit.com>
2020-02-25 12:28:15 -0600

    initial commit

---
+++ demo-angular/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { NsVersionNumber } from 'nativescript-ns-version-number';
+console.log(new NsVersionNumber().message);
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "ns-app",
+    templateUrl: "app.component.html"
+})
+export class AppComponent { }
---
+++ src/ns-version-number.android.ts
@@ -0,0 +1,5 @@
+import { Common } from './ns-version-number.common';
+
+export class NsVersionNumber extends Common {
+
+}
